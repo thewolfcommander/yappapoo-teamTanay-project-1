2d5e540d9744ab32ad8581fb16b80bb0ee845ace
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-04-23 18:02:10 +0530

    Starting the Backend of the Application

--- src/static/js/main.js
+++
@@ -1,6 +0,0 @@
-// tOGGLING WISHLIST Icon
-let wishlistIcon = document.querySelector('.wishlist-icon')
-
-$(wishlistIcon).click(function(e) {
-    wishlistIcon.classList.toggle('.wishlist-icon--active');
-});
